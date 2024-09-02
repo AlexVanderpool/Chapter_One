f08570f15fa65abad499fb715e3d905ae98f7571
AlexVanderpool <Alexvanderpool15@gmail.com>
2024-09-02 13:22:39 -0500

    It Works

---
+++ app/src/main/java/com/example/chapter_one/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.chapter_one
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.True)
+        falseButton = findViewById(R.id.False)
+
+
+        trueButton.setOnClickListener { Toast.makeText(this,"Correct", Toast.LENGTH_LONG).show() }
+        falseButton.setOnClickListener { Toast.makeText(this,"Incorrect", Toast.LENGTH_LONG).show() }
+
+    }
+}
