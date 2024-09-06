64797a796c6125ecf3fab0f9a0c2e651c8126cd4
AlexVanderpool <Alexvanderpool15@gmail.com>
2024-09-06 00:09:55 -0500

    Final exercise 2, switched toast for Snackbar

--- app/src/main/java/com/example/chapter_one/MainActivity.kt
+++ app/src/main/java/com/example/chapter_one/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,9 +19,11 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.True)
         falseButton = findViewById(R.id.False)
 
-
         trueButton.setOnClickListener { Toast.makeText(this,R.string.Correct, Toast.LENGTH_LONG).show() }
         falseButton.setOnClickListener { Toast.makeText(this,R.string.Incorrect, Toast.LENGTH_LONG).show() }
+
+        Snackbar.make(trueButton, "Correct", Snackbar.LENGTH_LONG).show()
+        Snackbar.make(falseButton, "Incorrect", Snackbar.LENGTH_LONG).show()
 
     }
 }
