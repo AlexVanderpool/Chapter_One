993ed440db67e17963cfd9560eb1ab6b05f5efb1
AlexVanderpool <Alexvanderpool15@gmail.com>
2024-09-14 17:22:04 -0500

    Chapter 2 - Getting questions to cycle through with it displaying correct rue and false to the question.

--- app/src/main/java/com/example/chapter_Two/MainActivity.kt
+++ app/src/main/java/com/example/chapter_Two/MainActivity.kt
@@ -5,15 +5,18 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
+import com.example.chapter_Two.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
 
+    private lateinit var binding: ActivityMainBinding
+
     private val questionBank = listOf(
-       Question(R.string.Question_Australia, true),
-       Question(R.string.Question_oceans, true),
+        Question(R.string.Question_Australia, true),
+        Question(R.string.Question_oceans, true),
         Question(R.string.Question_mideast, false),
         Question(R.string.Question_africa, false),
         Question(R.string.Question_americas, true),
@@ -26,14 +29,53 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        trueButton = findViewById(R.id.True)
-        falseButton = findViewById(R.id.False)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // trueButton = findViewById(R.id.True)
+        // falseButton = findViewById(R.id.False)
 
-        trueButton.setOnClickListener { Toast.makeText(this,R.string.Correct, Toast.LENGTH_LONG).show() }
-        falseButton.setOnClickListener { Toast.makeText(this,R.string.Incorrect, Toast.LENGTH_LONG).show() }
+        binding.True.setOnClickListener {
+         /*   Toast.makeText(this, R.string.Correct, Toast.LENGTH_SHORT).show() */
+            checkAnswer(true)
+        }
+        binding.False.setOnClickListener {
+            /* Toast.makeText(
+                this,
+                R.string.Incorrect,
+                Toast.LENGTH_SHORT
+            ).show()
+            */
+            checkAnswer(false)
+        }
 
-        Snackbar.make(trueButton, "Correct", Snackbar.LENGTH_LONG).show()
-        Snackbar.make(falseButton, "Incorrect", Snackbar.LENGTH_LONG).show()
+        binding.nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            //val questionTextResId = questionBank[currentIndex].textResId
+            //binding.questionTextView.setText(questionTextResId)
+            updateQuestion()
+        }
 
+        // val questionTextResId = questionBank[currentIndex].textResId
+        // binding.questionTextView.setText(questionTextResId)
+        updateQuestion()
+    }
+
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        binding.questionTextView.setText(questionTextResId)
+
+    }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        var correctAnswer = questionBank[currentIndex].answer
+
+        val messageResID = if (userAnswer == correctAnswer) {
+            R.string.Correct
+        } else {
+            R.string.Incorrect
+        }
+    Toast.makeText( this,
+        messageResID,
+        Toast.LENGTH_SHORT).show()
     }
 }
